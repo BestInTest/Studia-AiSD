6dbeebf392ca5d5521b8b62f464324612f81e30e
BestInTest <ziomal25565@gmail.com>
2025-12-02 15:42:19 +0100

    Notatki

--- main.cpp
+++ main.cpp
@@ -2,6 +2,64 @@
 
 using std::string;
 
+// zrobić (zmodyfikowane?) przejście wszerz
+// trzeba ograniczyć liczbę przejść wszerz
+// bfs?
+// są różne sposoby ograniczania przejść wszerz
+// nie dsu
+/*
+ * kod przejścia wszerz
+ * bfs(vector<vector<int>>& arr, int start) {
+ *     queue<int> q;
+ *     q.push(start);
+ *     vector<bool> visited(arr.size(), false);
+ *     visited[start] = true;
+ *
+ *     while (!q.empty()) {
+ *         int z = q.front();
+ *         cout << z << "\n";
+ *         q.pop();
+ *
+ *         for (int neighbor : arr[z]) {
+ *             if (!visited[neighbor]) {
+ *                 q.push(neighbor);
+ *                 visited[neighbor] = true;
+ *             }
+ *         }
+ *     }
+ * }
+ */
+
+/*
+ * kod przejścia wszerz z ograniczeniem głębokości/dystansu
+ * bfs(vector<vector<int>>& arr, int start) {
+ *     queue<int> q;
+ *     q.push(start);
+ *     vector<bool> visited(arr.size(), false);
+ *     visited[start] = true;
+ *     vector<int> dist(arr.size()-1);
+ *     dist[start] = 0;
+ *
+ *     while (!q.empty()) {
+ *         int z = q.front();
+ *         cout << z << "\n";
+ *         q.pop();
+ *
+ *         for (int neighbor : arr[z]) {
+ *             if (!visited[neighbor]) {
+ *                 q.push(neighbor);
+ *                 visited[neighbor] = true;
+ *                 dist[neighbor] = dist[z] + 1;
+ *             }
+ *             // tutaj albo za nawiasem ponizej jakis if ma byc
+ *         }
+ *     }
+ * }
+ */
+
+// bfs(vector<vector<int>>& graph, int start, int target, int max_depth)???
+
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cout.tie(nullptr);

a40199d0b326e1fabf99b588c04a36345cf01cfb
BestInTest <ziomal25565@gmail.com>
2026-01-18 18:52:03 +0100

    Zmiany

--- main.cpp
+++ main.cpp
@@ -4,57 +4,54 @@
 
 using namespace std;
 
-const int MOD = 1e9 + 7;
-long long pairs[100] = {};
+const long long MOD = 1e9 + 7;
 
-// Obliczanie wszystkich sposobów iloczynu dwóch cyfr
-void calcPairs() {
-    for (int i = 0; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++) {
-            pairs[i * j]++;
-        }
-    }
-}
+long long calcPairs(int result);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cout.tie(nullptr);
     std::cin.tie(nullptr);
-    calcPairs();
 
-    string inputStr;
-    cin >> inputStr;
+    string s;
+    cin >> s;
 
-    int len = inputStr.length();
-    vector<long long> dp(len + 1);
+    int n = s.length();
+    vector<long long> dp(n + 1, 0);
+    dp[n] = 1;
 
-    // pusty sufiks można uzyskać na 1 sposób (nic nie robiąc)
-    dp[len] = 1;
-
-    // Pętla od końca
-    for (int i = len - 1; i >= 0; i--) {
-        int num1 = inputStr[i] - '0'; // konwersja znaku na liczbe
-        long long possib = pairs[num1] * dp[i + 1];
+    // Petla od konca
+    for (int i = n - 1; i >= 0; i--) {
+        int num = s[i] - '0'; // Zamiana znaku na liczbe
+        long long possib = calcPairs(num) * dp[i + 1];
         dp[i] = (dp[i] + possib) % MOD;
 
-        // Sprawdzamy czy możemy wziąć dwie cyfry
-        if (i + 1 < len && inputStr[i] != '0') {
-            int dzies = (inputStr[i] - '0') * 10;
-            int jedn = inputStr[i + 1] - '0';
-            int num2 = dzies + jedn; // liczba z dwóch cyfr
-
-            possib = pairs[num2] * dp[i + 2];
+        // Bierzemy dwie cyfry jeśli się da
+        if (i + 1 < n && s[i] != '0') {
+            num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            possib = calcPairs(num) * dp[i + 2];
             dp[i] = (dp[i] + possib) % MOD;
-
         }
     }
 
     long long result = dp[0];
-    for (int i = 1; i < len; i++) {
+    for (int i = 1; i < n; i++) {
         result = (result + 2 * dp[i]) % MOD;
     }
 
     cout << result << endl;
 
     return 0;
+}
+
+long long calcPairs(int result) {
+    long long count = 0;
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 0; j <= 9; j++) {
+            if (i * j == result) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
